1372db4a2cb37fb9192c0e3f02613424fc22f175
Максим Деченков <Visond@rambler.ru>
2023-04-19 01:32:06 +0700

    Text hihi1

---
+++ pr-15-Dechenkov/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace pr_15_Dechenkov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext= this;
+        }
+
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mail = new System.Net.Mail.MailAddress(email);
+                return true;
+            }
+            catch{
+
+                return false;
+            }
+        }
+
+        private void txtMessage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ pr-15-Dechenkov/TestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using pr_15_Dechenkov;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TxtBoxValid()
+        {
+            string inText = "asd12";
+            string outText = "asd1";
+
+            MainWindow mainWindow = new();
+
+
+        }
+    }
+}
